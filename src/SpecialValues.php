4d0db183cadb14f202cce2d037e0c71e0753b2a2
Maxim Kuvardin <maxim@kuvard.in>
2023-09-15 01:36:02 +0600

    Added method notNullOrNull()

--- src/SpecialValues.php
+++ src/SpecialValues.php
@@ -30,6 +30,15 @@ class SpecialValues
         return null;
     }
 
+    public static function notNullOrNull(?bool $is_not_null): SpecialValues\IsNull|SpecialValues\NotNull|null
+    {
+        if ($is_not_null !== null) {
+            return $is_not_null ? self::notNull() : self::isNull();
+        }
+
+        return null;
+    }
+
     public static function setDefault(): SpecialValues\SetDefault
     {
         return new SpecialValues\SetDefault;
