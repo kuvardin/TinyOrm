99d28008e49d6ff3e752b7a49b3d26eb09e0aa82
Maxim Kuvardin <maxim@kuvard.in>
2024-08-01 19:07:32 +0500

    Expression conditions

--- src/SpecialValues.php
+++ src/SpecialValues.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace Kuvardin\TinyOrm;
 
+use Kuvardin\TinyOrm\SpecialValues\IsNull;
+
 /**
  * @package Kuvardin\TinyOrm
  * @author Maxim Kuvardin <maxim@kuvard.in>
  */
 class SpecialValues
 {
+    static ?IsNull $is_null = null;
+    static ?IsNull $not_null = null;
+
     private function __construct()
     {
 
@@ -17,12 +22,12 @@ class SpecialValues
 
     public static function isNull(): SpecialValues\IsNull
     {
-        return new SpecialValues\IsNull;
+        return self::$is_null ??= new SpecialValues\IsNull;
     }
 
     public static function notNull(): SpecialValues\NotNull
     {
-        return new SpecialValues\NotNull;
+        return self::$not_null ??= new SpecialValues\NotNull;
     }
 
     public static function nullOrNotNull(?bool $is_null): SpecialValues\IsNull|SpecialValues\NotNull|null
--- src/Traits/QueryConditionsListTrait.php
+++ src/Traits/QueryConditionsListTrait.php
@@ -5,14 +5,18 @@ declare(strict_types=1);
 namespace Kuvardin\TinyOrm\Traits;
 
 use Kuvardin\TinyOrm\Conditions\ConditionAbstract;
+use Kuvardin\TinyOrm\Conditions\ConditionExpression;
 use Kuvardin\TinyOrm\Conditions\ConditionsList;
 use Kuvardin\TinyOrm\Enums\LogicalOperator;
+use Kuvardin\TinyOrm\Expressions\ExpressionAbstract;
 
 trait QueryConditionsListTrait
 {
     public ConditionsList $conditions;
 
-    public function setWhere(ConditionAbstract|array|null $condition_item): self
+    public function setWhere(
+        ConditionAbstract|array|null $condition_item,
+    ): self
     {
         $this->conditions = is_array($condition_item)
             ? ConditionsList::fromValuesArray($condition_item)
@@ -21,7 +25,18 @@ trait QueryConditionsListTrait
         return $this;
     }
 
-    public function andWhere(ConditionAbstract|array $condition_item): self
+    public function setWhereExpression(
+        ExpressionAbstract $expression,
+        LogicalOperator $prefix = null,
+        bool $invert = null,
+    ): self
+    {
+        return $this->setWhere(new ConditionExpression($expression, $prefix, $invert));
+    }
+
+    public function andWhere(
+        ConditionAbstract|array $condition_item,
+    ): self
     {
         $this->conditions->append(
             is_array($condition_item)
@@ -30,6 +45,14 @@ trait QueryConditionsListTrait
         );
 
         return $this;
+    }
+
+    public function andWhereExpression(
+        ExpressionAbstract $expression,
+        bool $invert = null,
+    ): self
+    {
+        return $this->andWhere(new ConditionExpression($expression, invert: $invert));
     }
 
     public function orWhere(ConditionAbstract|array $condition_item): self
@@ -41,5 +64,13 @@ trait QueryConditionsListTrait
         );
 
         return $this;
+    }
+
+    public function orWhereExpression(
+        ExpressionAbstract $expression,
+        bool $invert = null,
+    ): self
+    {
+        return $this->orWhere(new ConditionExpression($expression, invert: $invert));
     }
 }
