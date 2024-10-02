7f8b8f60734ebe6098246c7913952b1f98666d12
Maxim Kuvardin <maxim@kuvard.in>
2024-10-02 15:58:06 +0500

    Bug fixed

--- src/Enums/JoinType.php
+++ src/Enums/JoinType.php
@@ -7,7 +7,10 @@ namespace Kuvardin\TinyOrm\Enums;
 enum JoinType: string
 {
     case Inner = 'INNER';
+    case Left = 'LEFT';
     case LeftOuter = 'LEFT OUTER';
+    case Right = 'RIGHT';
     case RightOuter = 'RIGHT OUTER';
+    case Full = 'FULL';
     case FullOuter = 'FULL OUTER';
 }
